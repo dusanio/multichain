43997414ed37ce1f17e64659e5168e5980d7a58c
Dushan <trajkodulic@gmail.com>
2023-04-07 18:36:49 +0200

    Updates

--- script.js
+++
@@ -1,6 +0,0 @@
-const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
-
-tl.to(".text", { y: "0%", duration: 2, stagger: 0.25 });
-tl.to("#slider", { y: "-100%", duration: 1, delay: 0.5 });
-tl.to("#intro", { y: "-100%", duration: 1 }, "-=0.8");
-tl.fromTo("#header", { opacity: 0 }, { opacity: 1, duration: 0.25 });
